67baa256ed94e052bb108e6a0bfab34bfacd60b5
sykim <sykim@sykimui-MacBookPro.local>
2025-02-25 01:28:36 +0900

    modified split_data.sh

--- simulator/traces/split_data.sh
+++ simulator/traces/split_data.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-ROOT="./paddr/"
-INPUT="$ROOT$1/$1_5m.paddr"
-echo "$INPUT"
-echo `head -4000000 $INPUT > $ROOT$1/$1_4m.paddr`
-echo `head -3000000 $INPUT > $ROOT$1/$1_3m.paddr`
-echo `tail -1000000 $INPUT > $ROOT$1/$1_1m_tail.paddr`
+ROOT="./paddr"
+DIRS=("astar" "bc" "bfs" "cc" "mcf" "pr" "sssp")
+
+for dir in "${DIRS[@]}"; do
+    INPUT="$ROOT/$dir/${dir}_5m.paddr"
+
+    if [ -f "$INPUT" ]; then
+        echo "Processing $INPUT"
+
+        head -4000000 "$INPUT" > "$ROOT/$dir/${dir}_4m.paddr"
+        head -3000000 "$INPUT" > "$ROOT/$dir/${dir}_3m.paddr"
+        tail -1000000 "$INPUT" > "$ROOT/$dir/${dir}_1m_tail.paddr"
+
+        echo "Created ${dir}_4m.paddr, ${dir}_3m.paddr, and ${dir}_1m_tail.paddr in $dir"
+    else
+        echo "File $INPUT not found, skipping..."
+    fi
+done
